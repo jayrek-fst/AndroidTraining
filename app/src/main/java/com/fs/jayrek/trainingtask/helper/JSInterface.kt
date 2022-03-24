533e7603e9ee0b4b04d14b0f0c44a14b7bcd3e2d
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2022-03-24 22:55:24 +0800

    -webview integration

---
+++ app/src/main/java/com/fs/jayrek/trainingtask/helper/JSInterface.kt
@@ -0,0 +1,19 @@
+package com.fs.jayrek.trainingtask.helper
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.webkit.JavascriptInterface
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
+import com.fs.jayrek.trainingtask.view.activity.SignUpWebViewActivity
+
+class JSInterface(private val mContext: Context) {
+
+    @JavascriptInterface
+    fun signup(){
+        Log.wtf("test:", "url")
+//        val intent = Intent(mContext, SignUpWebViewActivity::class.java)
+//        startActivity(intent)
+    }
+}
--- app/src/main/java/com/fs/jayrek/trainingtask/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/fs/jayrek/trainingtask/view/fragment/HomeFragment.kt
@@ -1,24 +1,48 @@
 package com.fs.jayrek.trainingtask.view.fragment
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.*
 import androidx.fragment.app.Fragment
 import com.fs.jayrek.trainingtask.R
+import com.fs.jayrek.trainingtask.helper.JSInterface
 import com.fs.jayrek.trainingtask.helper.StringHelper
 
 
 class HomeFragment : Fragment() {
 
-    @SuppressLint("SetJavaScriptEnabled")
+    @SuppressLint("SetJavaScriptEnabled", "JavascriptInterface")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val webView = view.findViewById<WebView>(R.id.webView)
+        webView.loadUrl(StringHelper.homeWebUrl)
+
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (Uri.parse(url).host == StringHelper.homeWebUrl) {
+                    return false
+                }
+
+                Intent(Intent.ACTION_VIEW, Uri.parse(url)).apply {
+                    startActivity(this)
+                    return true
+                }
+            }
+        }
+        webView.addJavascriptInterface(JSInterface(requireActivity()), "Android")
 
         return view
     }
